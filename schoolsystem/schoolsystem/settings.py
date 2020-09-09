ad35f090f42095f2e4054002da8113e1c8b43aa1
AmbyMbayi <ambymbayi54@gmail.com>
2020-09-09 15:49:47 +0300

    first commit

--- schoolsystem/schoolsystem/settings.py
+++ schoolsystem/schoolsystem/settings.py
@@ -120,11 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/static')
+
+print(STATIC_ROOT)
+
+STATIC_URL = '/'
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
+
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
